54e0037d66442fa4ceb24f29b91d90c268106d77
haojianfeng <1005881431@qq.com>
2022-04-10 11:34:56 +0800

    fix: umi-dva

---
+++ src/pages/users/data.d.ts
@@ -0,0 +1,11 @@
+export interface SingleUserType {
+    "id": number,
+    "name": string,
+    "email": string,
+    "create_time": string,
+    "update_time": string,
+    "status": number
+}
+interface FormValues {
+    [name: string]: any
+}
